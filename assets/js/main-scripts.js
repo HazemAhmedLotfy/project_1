b2a034085fcd6e6143d6e83f1fa7c4101d9d25b3
HazemAhmedLotfy <30853910+HazemAhmedLotfy@users.noreply.github.com>
2021-07-29 04:55:22 +0200

    Add files via upload

--- assets/js/main-scripts.js
+++ assets/js/main-scripts.js
@@ -21,7 +21,8 @@ document.getElementById("defaultOpen").click();
 $('.logosSlider').owlCarousel({
     loop: true,
     margin: 0,
-    autoplay: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
     smartSpeed: 500,
     nav: true,
     dots: false,
@@ -41,25 +42,26 @@ $('.logosSlider').owlCarousel({
 });
 
 $('.clientSlider').owlCarousel({
-  loop: false,
-  margin: 0,
-  autoplay: false,
-  smartSpeed: 500,
-  nav: true,
-  dots: false,
-  navText: ['<i class="icon-arrow-down"></i>','<i class="icon-arrow-down"></i>'],
-  lazyLoad:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:4
-      },
-      1000:{
-          items:7
-      }
-  }
+    loop: false,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 500,
+    nav: true,
+    dots: false,
+    navText: ['<i class="icon-arrow-down"></i>','<i class="icon-arrow-down"></i>'],
+    lazyLoad:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:7
+        }
+    }
 });
 
 $('.whyUsSlider').owlCarousel({
